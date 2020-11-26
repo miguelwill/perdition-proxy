d20ab3ddbbb3c997710b7f70103d5711c77bac68
miguelwill <miguelwill@gmail.com>
2020-11-26 11:58:17 -0300

    remove ssh run

--- main.sh
+++ main.sh
@@ -8,17 +8,11 @@ KEY="/etc/perdition/perdition.key.pem"
 
 makegdbm /etc/perdition/popmap.gdbm.db < /etc/perdition/popmap
 
-/etc/init.d/rsyslog start 
-
-mkdir -m600  /run/sshd
-/etc/init.d/ssh start
+/etc/init.d/rsyslog start
 
 /etc/init.d/perdition start
 
-#/usr/sbin/perdition -d -f /etc/perdition/perdition.conf 
+#/usr/sbin/perdition -d -f /etc/perdition/perdition.conf
 
 
 tail -f /var/log/syslog
-
-
-
