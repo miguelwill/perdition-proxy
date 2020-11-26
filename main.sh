dc29cc82f0f7c5e92228b04be2fe0c72c12e9d70
miguelwill <miguelwill@gmail.com>
2020-11-26 18:13:40 -0300

    add rm pid files

--- main.sh
+++ main.sh
@@ -7,6 +7,12 @@ makegdbm /etc/perdition/popmap.gdbm.db < /etc/perdition/popmap
 
 /etc/init.d/rsyslog start
 
-/etc/init.d/perdition restart
+rm -v /var/run/perdition.imap4/perdition.imap4.pid
+rm -v /var/run/perdition.imaps/perdition.imaps.pid
+rm -v /var/run/perdition.managesieve/perdition.managesieve.pid
+rm -v /var/run/perdition.pop3/perdition.pop3.pid
+rm -v /var/run/perdition.pop3s/perdition.pop3s.pid
+
+/etc/init.d/perdition start
 
 tail -f /var/log/syslog
