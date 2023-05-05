c5120e87f199a47f8e0755683ac1f98efcd0b2f2
miguelwill <miguelwill@gmail.com>
2023-05-05 14:49:11 -0400

    update ubuntu and syslog config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:bionic
+FROM ubuntu:jammy
 
 LABEL maintainer "miguelwill@gmail.com"
 
--- main.sh
+++ main.sh
@@ -13,7 +13,9 @@ fi
 
 makegdbm /etc/perdition/popmap.gdbm.db < /etc/perdition/popmap
 
-/etc/init.d/rsyslog start
+/usr/sbin/rsyslogd -n -iNONE &
+touch /var/log/syslog
+chown syslog.adm /var/log/syslog
 
 rm -v /var/run/perdition.imap4/perdition.imap4.pid
 rm -v /var/run/perdition.imaps/perdition.imaps.pid
@@ -21,6 +23,6 @@ rm -v /var/run/perdition.managesieve/perdition.managesieve.pid
 rm -v /var/run/perdition.pop3/perdition.pop3.pid
 rm -v /var/run/perdition.pop3s/perdition.pop3s.pid
 
-/etc/init.d/perdition start
+/etc/init.d/perdition start &
 
 tail -f /var/log/syslog
